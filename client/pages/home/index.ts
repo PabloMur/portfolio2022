f64091650f71079e2e305a1e9dace37df1205966
Pablo <pablomurillo.sp@gmail.com>
2022-03-29 00:35:01 -0300

    probando animaciones

--- client/pages/home/index.ts
+++ client/pages/home/index.ts
@@ -10,8 +10,15 @@ class HomePage extends HTMLElement {
         <div class="home-page">
             <custom-menu></custom-menu>
             <div class="banner">
-                <custom-title>Hi, I am Pablo!</custom-title>
-                <h3>full-stack developer</h3>
+                <custom-title>Hola, soy Pablo!</custom-title>
+                <p class="banner-parrafo">
+                  Estudiante de la carrera de Desarrollador Web Full-Stack en <a href="https://apx.school/" target="_blank">APX</a>.
+                  </br>
+                  Este es mi portafolio, espero que lo disfruten!
+                </p>
+                <div class="cicleTwo"></div>
+                <div class="cicleOne"></div>
+                <div class="cicleThree"></div>
             </div>
             <custom-footer></custom-footer>
         </div>
@@ -41,6 +48,9 @@ class HomePage extends HTMLElement {
           font-size: 80px;
           font-weight: 700;
         }
+        .banner-parrafo{
+          font-size:24px;
+        }
         .cicleOne{
             position:absolute;
             height:300px;
@@ -49,6 +59,38 @@ class HomePage extends HTMLElement {
             border-radius:200px;
             top:-150px;
             right:-150px;
+            animation: 6s flotar;
+        }
+        .cicleTwo{
+          position:absolute;
+          height:400px;
+          width:400px;
+          background-color:#1ed760;
+          border-radius:200px;
+          top:130px;
+          right:-150px;
+          animation: 10s flotar;
+        }
+        .cicleThree{
+          position:absolute;
+          height:600px;
+          width:600px;
+          background-color:#1ed760;
+          border-radius:400px;
+          top:400px;
+          left:-200px;
+          animation: 7s flotar;
+      }
+        @keyframes flotar{
+          from{
+            opacity:0.5;
+            top: 1150px;
+          }
+          to{
+            opacity:1
+            top: -150px;
+          }
+        }
     
     `;
     this.appendChild(style);
