25d962c5ebe0151a4a1a9a29a217ff45f334742c
Pablo <polillomurillo@gmail.com>
2022-06-17 11:31:31 -0300

    fixing dist

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,14 @@
 import * as express from "express";
+import * as path from "path";
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("dist"));
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/dist/index.html");
-}),
-  app.listen(port, () => {
-    console.log("Iniciado en el puerto " + port);
-  });
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Iniciado en el puerto " + port);
+});
